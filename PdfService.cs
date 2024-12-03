fd82c90398f9cab50c0f5577d44d65c47074f4eb
Makar Shchelkunov <turumberum@gmail.com>
2024-12-03 20:35:14 +0300

    comp crc in log file, add converter

---
+++ PdfService.cs
@@ -0,0 +1,43 @@
+﻿namespace ActivationReport
+{
+    using iText.Kernel.Font;
+    using iText.Kernel.Pdf;
+    using iText.Layout;
+    using iText.Layout.Element;
+    using System.Collections.Generic;
+    using System.IO;
+
+    public class PdfService
+    {
+        /// <summary>
+        /// Создает PDF-файл с содержимым из списка строк.
+        /// </summary>
+        /// <param name="lines">Список строк для добавления в PDF.</param>
+        /// <param name="outputPath">Путь для сохранения PDF-файла.</param>
+        public void CreatePdfFromList(List<string> lines, string outputPath)
+        {
+            // Создаем поток для записи файла
+            using (FileStream stream = new FileStream(outputPath, FileMode.Create, FileAccess.Write))
+            {
+                // Создаем PDF-документ
+                PdfWriter writer = new PdfWriter(stream);
+                PdfDocument pdf = new PdfDocument(writer);
+                Document document = new Document(pdf);
+
+                // строчки кода где используется шрифт поддерживающий русский язык
+                var font = PdfFontFactory.CreateFont("C:\\Windows\\Fonts\\arial.ttf", "Identity-H");
+                document.SetFont(font);
+
+                // Добавляем строки в документ
+                foreach (string line in lines)
+                {
+                    document.Add(new Paragraph(line));
+                    //document.Add(new AreaBreak()); // Разрыв страницы
+                }
+
+                // Закрываем документ
+                document.Close();
+            }
+        }
+    }
+}
