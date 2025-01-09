87d5385bc19e859354f382bd00e7b80556004c4e
Makar Shchelkunov <turumberum@gmail.com>
2025-01-09 16:12:20 +0300

    fix search

---
+++ wwwroot/js/resizer.js
@@ -0,0 +1,4 @@
+﻿window.adjustTextareaHeight = function (textareaElement) {
+    textareaElement.style.height = 'auto';  // сбрасываем высоту, чтобы она подстроилась под контент
+    textareaElement.style.height = (textareaElement.scrollHeight) + 'px';  // устанавливаем высоту в зависимости от содержимого
+}
